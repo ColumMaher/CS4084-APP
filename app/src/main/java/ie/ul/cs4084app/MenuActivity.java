879446c377b5d9c535db1ddfbf4757479b71b04b
seanfitz121 <63353392+seanfitz121@users.noreply.github.com>
2022-04-24 20:33:16 +0100

    Update MenuActivity.java
    Change adapter usage

--- app/src/main/java/ie/ul/cs4084app/MenuActivity.java
+++ app/src/main/java/ie/ul/cs4084app/MenuActivity.java
@@ -1,24 +1,35 @@
 package ie.ul.cs4084app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MenuActivity extends AppCompatActivity {
     private ListView menuListView;
-
+    TextView quantity;
+    int quantity1 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        //quantity = (TextView) findViewByID(R.id.quantity);
 
         menuListView = findViewById(R.id.menuListView);
         menuListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
@@ -28,26 +39,55 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-        Restaurant mcDonalds = new Restaurant("McDonalds1", "1111@mail.com", "1244", "McDonalds \nCRESCENT SHOPPING CENTRE LIMERICK, V94H029");
-        mcDonalds.addMenuItem("Pizza", 4.0);
-        mcDonalds.addMenuItem("7up", 3.0);
-        mcDonalds.addMenuItem("Chicken", 3.5);
-        mcDonalds.getMenu();
+        MenuItem a = new MenuItem("Burger", 2.50);
+        MenuItem b = new MenuItem("Sprite", 2.00);
+        MenuItem c = new MenuItem("Fries", 1.50);
+        MenuItem d = new MenuItem("Ice Cream", 3.50);
 
-        //ArrayList of menu items
-        ArrayList<String> selectedMenu= new ArrayList<>();
-        for(String s : mcDonalds.getMenu().keySet()){
-            selectedMenu.add(s);
-        }
-        //ArrayList of menu item prices
-        ArrayList<Object> selectedMenuPrice= new ArrayList<>();
-        for(Object o : mcDonalds.getMenu().values()){
-            selectedMenuPrice.add(o);
-        }
-        //System.out.println(selectedMenu);
-        //System.out.println(selectedMenuPrice);
+        ArrayList<MenuItem> menuList = new ArrayList<>();
+        menuList.add(a);
+        menuList.add(b);
+        menuList.add(c);
+        menuList.add(d);
 
-        MenuAdapter menuAdapter = new MenuAdapter(this,R.layout.list_item_menu,selectedMenu);
+
+
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayAdapter adapter2 = new ArrayAdapter<String>(this, R.layout.list_item_menu, list1);
+
+        MenuAdapter menuAdapter = new MenuAdapter(this,R.layout.list_item_menu, menuList);
+
         menuListView.setAdapter(menuAdapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance("https://cs4084app-29f54-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Restaurant").child("McDonalds"/*RESTAURANT NAME HERE*/).child("Menu");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list1.add(snapshot.getValue().toString());
+                }
+                adapter2.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    //Increment/Decrement quantity
+    public void increment(View v){
+        //quantity1++;
+        //quantity.setText("" + quantity1);
+    }
+    public void decrement(View v){
+        /*if(quantity1 <= 0){
+            quantity1 = 0;
+        } else {
+            quantity1--;
+        }
+        quantity.setText("" + quantity1);*/
     }
 }
